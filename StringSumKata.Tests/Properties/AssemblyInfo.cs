0d5c2531022c833eac40d45c724c7af4514b9345
Pavel Verashchaka <Pavel_Verashchaka@epam.com>
2021-02-12 14:27:25 +0300

    UnitTestingTask: StringSumKata init

---
+++ StringSumKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringSumKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("StringSumKata.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2631909c-7b42-4c77-9867-c12d31d60928")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringSumKata.Tests/UnitTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringSumKata;
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace StringSumKata.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [Fact]
+        public void Should_Throw_ArgumentNullException_If_Argument_Is_Null()
+        {
+            StringSumUtility ssu = new StringSumUtility();
+            Action action = () => ssu.Sum(null, "1");
+
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentException_If_Argument_Is_Empty()
+        {
+            StringSumUtility ssu = new StringSumUtility();
+            Action action = () => ssu.Sum("", "1");
+
+            action.Should().ThrowExactly<ArgumentException>();
+        }
+
+        [Fact]
+        public void Should_Return_Zero_If_Both_Nums_Not_Natural()
+        {
+            StringSumUtility ssu = new StringSumUtility();
+            var result = ssu.Sum("asd", "asd");
+
+            result.Should().Be("0");
+        }
+    }
+}
