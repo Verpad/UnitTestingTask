2551a980fa32110907ca203810537bb57cae1e0a
Pavel Verashchaka <Pavel_Verashchaka@epam.com>
2021-02-12 16:03:05 +0300

    FizzBuzzKata: Initial tests and utility init

---
+++ FizzBuzzKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzzKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("FizzBuzzKata.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0699b4c-13d2-4dcf-a181-965f4bf69bac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FizzBuzzKata.Tests/UnitTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FizzBuzzKata;
+using Xunit;
+using FluentAssertions;
+
+namespace FizzBuzzKata.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Should_Return_100_Elements()
+        {
+            FizzBuzzUtility fbu = new FizzBuzzUtility();
+            int elementsCount = fbu.GenerateElements().Length;
+
+            elementsCount.Should().Be(100);
+        }
+
+        [Fact]
+        public void Should_Return_Fizz_At_Every_Third_Position()
+        {
+            FizzBuzzUtility fbu = new FizzBuzzUtility();
+            string[] elements = fbu.GenerateElements();
+
+            bool isRight = true;
+            for (int i = 2; i < elements.Length; i = i + 3)
+            {
+                if (elements[i] != "Fizz")
+                {
+                    isRight = false;
+                    break;
+                }
+            }
+
+            isRight.Should().Be(true);
+        }
+
+        [Fact]
+        public void Should_Return_Buzz_At_Every_Fifth_Position()
+        {
+            FizzBuzzUtility fbu = new FizzBuzzUtility();
+            string[] elements = fbu.GenerateElements();
+
+            bool isRight = true;
+            for (int i = 4; i < elements.Length; i = i + 5)
+            {
+                if (elements[i] != "Buzz")
+                {
+                    isRight = false;
+                    break;
+                }
+            }
+
+            isRight.Should().Be(true);
+        }
+
+        [Fact]
+        public void Should_Return_FizzBuzz_At_Every_Fifteenth_Position()
+        {
+            FizzBuzzUtility fbu = new FizzBuzzUtility();
+            string[] elements = fbu.GenerateElements();
+
+            bool isRight = true;
+            for (int i = 14; i < elements.Length; i = i + 15)
+            {
+                if (elements[i] != "FizzBuzz")
+                {
+                    isRight = false;
+                    break;
+                }
+            }
+
+            isRight.Should().Be(true);
+        }
+    }
+}
